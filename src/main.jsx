8667dceefab8b5812ed7a7a5b034567f35f12b8c
PuiIoan <ioan.pui@smartboxdigital.com>
2023-03-25 11:56:49 +0200

    add redux config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from "react";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./store";
+import persistStore from "redux-persist/es/persistStore";
+// ==============================|| REACT DOM RENDER  ||============================== //
+let persistor = persistStore(store);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root")); // createRoot(container!) if you use TypeScript
+root.render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+);
